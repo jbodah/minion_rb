e7fb92a8d3d78b1278587aa9e8115d30967982ac
Josh Bodah <joshuabodah@gmail.com>
2018-07-18 14:40:45 -0400

    actually add some code to get to mvp

--- lib/minion.rb
+++ lib/minion.rb
@@ -1,5 +1,57 @@
 require "minion/version"
+require "evil-proxy"
 
 module Minion
-  # Your code goes here...
+  def self.proxy(&block)
+    Minion::Instance.new.tap { |m| m.instance_eval(&block) }
+  end
+
+  class Instance
+    def initialize
+      @hosts = {}
+    end
+
+    def host(name, &block)
+      host = Minion::Host.new.tap { |h| h.instance_eval(&block) }
+      @hosts[name] = host
+    end
+
+    def start
+      mitm_pattern = /^(#{@hosts.keys.map { |hostname| Regexp.escape(hostname) }.join('|')})$/
+      proxy = EvilProxy::MITMProxyServer.new Port: 8080, MITMPattern: mitm_pattern
+      # todo - unsuckify
+      @hosts.each do |hostname, minion_host|
+        proxy.start_mitm_server(hostname, 0)
+        proxy.instance_eval { minion_host.apply(@mitm_servers[hostname]) }
+      end
+      proxy.start
+    end
+  end
+
+  class Host
+    def initialize
+      @handlers = {}
+    end
+
+    def get(expr, &block)
+      @handlers[:get] ||= []
+      @handlers[:get] << [expr, block]
+    end
+
+    def apply(mitm_server)
+      @handlers.each do |method, cases|
+        mitm_server.instance_eval do
+          define_singleton_method "do_#{method.upcase}" do |req|
+            match = cases.find { |(expr, _)| req.path =~ expr }
+            if match.nil?
+              super
+            else
+              _, handler = match
+              handler.call(req)
+            end
+          end
+        end
+      end
+    end
+  end
 end
